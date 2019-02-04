b3a829943d5eff6d6ad47b573fac318938280d00
Enis Inan <enis.inan@puppet.com>
2019-02-03 22:27:12 -0800

    Push-up solution to Exercise 3

--- 3-limit-service-time/main.go
+++ 3-limit-service-time/main.go
@@ -10,21 +10,91 @@
 
 package main
 
+import "time"
+import "sync"
+//import "fmt"
+
+var maxTime int64
+var users chan *User
+
 // User defines the UserModel. Use this to check whether a User is a
 // Premium user or not
 type User struct {
 	ID        int
 	IsPremium bool
+
 	TimeUsed  int64 // in seconds
+	mux       sync.Mutex // for accessing the shared variable TimeUsed
+}
+
+func (u *User) IncrementTimeUsed() int64 {
+	u.mux.Lock()
+	defer u.mux.Unlock()
+
+	u.TimeUsed++
+	return u.TimeUsed
 }
 
 // HandleRequest runs the processes requested by users. Returns false
 // if process had to be killed
 func HandleRequest(process func(), u *User) bool {
-	process()
-	return true
+	if u.IsPremium {
+		// Premium users have unlimited access, so no need
+		// to keep track of their time.
+		process()
+		return true
+	}
+	// We have a free user.
+
+	// Possible data-race, but not a big deal because
+	// TimeUsed is always incremented + the writes are
+	// thread-safe
+	if u.TimeUsed >= maxTime {
+		return false
+	}
+
+	timesUp := make(chan bool)
+	processDone := make(chan bool)
+
+	go func() {
+		processStarted := make(chan bool)
+		go func() {
+			processStarted <- true
+			process()
+
+			// close to broadcast a signal
+			// to both the ticking goroutine
+			// and the handler's goroutine
+			close(processDone)
+		}()
+		<-processStarted
+
+		for {
+			select {
+			case <-processDone:
+				return
+			default:
+				time.Sleep(1 * time.Second)
+				timeUsed := u.IncrementTimeUsed()
+				if timeUsed >= maxTime {
+					timesUp <- true
+					return
+				}
+			}
+		}
+	}()
+
+	for {
+		select {
+		case <-timesUp:
+			return false
+		case <-processDone:
+			return true
+		}
+	}
 }
 
 func main() {
+	maxTime = 16
 	RunMockServer()
 }
